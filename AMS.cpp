475cd9e72c9fe071a16076a7efcb2acdf1d6b2fb
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-04-07 11:17:48 +0800

    Beeno update magic numbers, loadfile functionsss

--- AMS.cpp
+++ AMS.cpp
@@ -2,6 +2,10 @@
  * AMS v0.1
  */
 
+// magic numbers
+#define stdfile="StudentRecords..txt"
+#define profile="ProgrammeRecords.txt"
+
 // including header
 
 #include <iostream>
@@ -12,6 +16,9 @@
 #include <sstream>
 #include <fstream>
 #include <ctime>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 using namespace std;
 
@@ -57,26 +64,31 @@ string center(string target);
 void delay(int time);
 
 void warming(string);
+int leave(string msg);
 
-ifstream loadfile(string filename,int count);
-ifstream loadfile(string filename);
+bool fexists(const char *filename);
+
+bool loadifile(ifstream &file, const char *filename, int count);
+bool loadifile(ifstream &file, const char *filename);
+bool loadofile(ifstream &file, const char *filename, int count);
+bool loadofile(ifstream &file, const char *filename);
 
 
 // declearing class
 
 class GroupMemberRec{
 public:
-   string name,id;
+    string name,id;
 };
 
 class GroupMemberClass
 {
 public:
-   GroupMemberClass();
-   void add(string name,string id);
-   void show();
+    GroupMemberClass();
+    void add(string name,string id);
+    void show();
 private:
-   vector<GroupMemberRec> GroupMemberList;
+    vector<GroupMemberRec> GroupMemberList;
 };
 
 
@@ -99,117 +111,169 @@ GroupMemberClass GroupMember;
 
 string to_string (int a)
 {
-   ostringstream temp;
-   temp<<a;
-   return temp.str();
+    ostringstream temp;
+    temp<<a;
+    return temp.str();
 }
 
 #ifdef linux
 void goup(){
-   cout << "\x1b[A";
+    cout << "\x1b[A";
 }
 void goup(int n){
-   cout << "\x1b["+to_string(n)+"A";
+    cout << "\x1b["+to_string(n)+"A";
 }
 void godown(){
-   cout << "\x1b[B";
+    cout << "\x1b[B";
 }
 void godown(int n){
-   cout << "\x1b["+to_string(n)+"B";
+    cout << "\x1b["+to_string(n)+"B";
 }
 void goright(){
-   cout << "\x1b[C";
+    cout << "\x1b[C";
 }
 void goright(int n){
-   cout << "\x1b["+to_string(n)+"C";
+    cout << "\x1b["+to_string(n)+"C";
 }
 void goleft(){
-   cout << "\x1b[D";
+    cout << "\x1b[D";
 }
 void goleft(int n){
-   cout << "\x1b["+to_string(n)+"D";
+    cout << "\x1b["+to_string(n)+"D";
 }
 string prel(){
-   return "\x1b[F";
+    return "\x1b[F";
 }
 void gorow(int n){
-   cout << "\x1b"+to_string(n)+"[G";
+    cout << "\x1b"+to_string(n)+"[G";
 }
 void gotoxy(int x, int y){
-   cout << "\x1b["+to_string(y)+';'+to_string(x)+"H";
+    cout << "\x1b["+to_string(y)+';'+to_string(x)+"H";
 }
 #elif _WIN32
 #include <windows.h>
 void gotoxy(int xpos, int ypos){
-   COORD scrn;
-   HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
-   scrn.X = xpos; scrn.Y = ypos;
-   SetConsoleCursorPosition(hOuput,scrn);
+    COORD scrn;
+    HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+    scrn.X = xpos; scrn.Y = ypos;
+    SetConsoleCursorPosition(hOuput,scrn);
 }
 #endif
 
 string man_string(string ori,int n)
 {
-   string temp="";
-   for(int i=0;i<n;i++)
-   {
-      temp+=ori;
-   }
-   return temp;
+    string temp="";
+    for(int i=0;i<n;i++)
+    {
+        temp+=ori;
+    }
+    return temp;
 }
 
 string space(int n,char c)
 {
-   string temp="";
-   for(int i=0;i<n;i++)
-   {
-      temp+=c;
-   }
-   return temp;
+    string temp="";
+    for(int i=0;i<n;i++)
+    {
+        temp+=c;
+    }
+    return temp;
 }
 string space(int n)
 {
-   return space(n,' ');
+    return space(n,' ');
 }
 
 string lsr(string l,char c,string r,int n)
 {
-   return (l+space((n-l.length()-r.length()),c)+r);
+    return (l+space((n-l.length()-r.length()),c)+r);
 }
 string lsr(string l,string r,int n)
 {
-   return lsr(l,' ',r,n);
+    return lsr(l,' ',r,n);
 }
 string lsr(string l,string r)
 {
-   return lsr(l,r,80);
+    return lsr(l,r,80);
 }
 
 string center(string target,int count,char filling){
-   while (target.length()<(unsigned)count) {
-      target=filling+target;
-      if (target.length()<(unsigned)count) target += filling;
-   }
-   return target;
+    while (target.length()<(unsigned)count) {
+        target=filling+target;
+        if (target.length()<(unsigned)count) target += filling;
+    }
+    return target;
 }
 string center(string target,int count){
-   return center(target,count,' ');
+    return center(target,count,' ');
 }
 string center(string target,char filling){
-   return center(target,80,filling);
+    return center(target,80,filling);
 }
 string center(string target){
-   return center(target,80,' ');
+    return center(target,80,' ');
 }
 
 void delay(int time){
-   clock_t now=clock();
+    clock_t now=clock();
 
-   while(clock()-now<time);
+    while(clock()-now<time);
 }
 
 void warming(string msg){
-   cout<<msg;
+    cout<<msg;
+}
+
+int leave(string msg){
+    cout<<"\nEnter a char to leave...";
+    char c;
+    cin>>c;
+    cout<<endl<<center(msg);
+    return 1;
+}
+
+bool fexists(const char *filename) {
+  ifstream ifile(filename);
+  return ifile;
+}
+
+bool loadifile(ifstream &file, const char *filename, int count){        
+    if (fexists(filename)){
+        file.open(filename);
+        return file.is_open();
+    } else {
+        string msg="\n"+(string)filename+" is currently not readable!";
+        if (count>1){
+            msg+=" retrying "+to_string(--count)+" time";
+            msg=(count>1)?msg+"s":msg;
+            warming(msg);
+            return loadifile(file,filename,count);
+        } else {
+            warming(msg+" Please check!");
+            return false;
+        }
+    }
+}
+bool loadifile(ifstream &file, const char *filename){
+    return loadifile(file,filename,10);
+}
+bool loadofile(ifstream &file, const char *filename, int count){
+    if (fexists(filename)){
+        file.open(filename);
+        return file.is_open();
+    } else {
+        string msg="\n"+(string)filename+"not accessable!";
+        if (count>1){
+            msg+=" retrying "+to_string(count)+" time";
+            msg=(count>1)?msg:msg+"s";
+            return loadifile(file,filename,count);
+        } else {
+            return false;
+        }
+    }
+}
+bool loadofile(ifstream &file, const char *filename){
+    return loadofile(file,filename,10);
 }
 
 
@@ -220,35 +284,49 @@ void warming(string msg){
 // defining class
 
 GroupMemberClass::GroupMemberClass(void){
-   this->add("Tung Cheung Leong","13058536A");
-   this->add("Tai Leung Kin","12171480A");
+    this->add("Tung Cheung Leong","13058536A");
+    this->add("Tai Leung Kin","12171480A");
 }
 
 void GroupMemberClass::add(string name,string id){
-   GroupMemberRec newmember;
-   newmember.name=name;
-   newmember.id=id;
-   GroupMemberList.push_back(newmember);
+    GroupMemberRec newmember;
+    newmember.name=name;
+    newmember.id=id;
+    GroupMemberList.push_back(newmember);
 }
 
 void GroupMemberClass::show(){
-   cout<<endl<<center(center("Group Member List",40,'-'))<<endl;
-   for(unsigned int i=0;i<GroupMemberList.size();i++){
-      cout<<center(lsr(GroupMemberList[i].name,GroupMemberList[i].id,40))<<endl;
-   }
+    cout<<endl<<center(center("Group Member List",40,'-'))<<endl;
+    for(unsigned int i=0;i<GroupMemberList.size();i++){
+        cout<<center(lsr(GroupMemberList[i].name,GroupMemberList[i].id,40))<<endl;
+    }
 }
 
 
 // defining main functions
 
 void welcome(){
-   cout<<center("Welcome!",'=');
-   cout<<center("CCN2042 Computer Programming");
-   cout<<center("Group Assignment - Articulation Management System");
+    cout<<center("Welcome!",'=');
+    cout<<center("CCN2042 Computer Programming");
+    cout<<center("Group Assignment - Articulation Management System");
 
-   cout<<endl;
-   GroupMember.show();
-   cout<<center(center("-",40,'-'));
+    cout<<endl;
+    GroupMember.show();
+    cout<<center(center("-",40,'-'));
+}
+
+void loadfromfile(){
+    cout<<"loadfilefile";
+    ifstream stdfile;
+    if (loadifile(stdfile,"StudentRecords..txt")) {
+        cout<<"\nok";
+    } else {
+        cout<<"\nnot ok";
+    }
+/*
+ProgrammeRecords.txt
+StudentRecords.txt
+*/
 }
 
 
@@ -261,16 +339,13 @@ void welcome(){
 
 int main()
 {
-   welcome();
+    welcome();
 
-   cout<<man_string("\n",2);
+    cout<<man_string("\n",2);
 
-   ///loadfromfile();
+    loadfromfile();
 
 
-   leave();
-   cout<<center("normal end");
-   char c;
-   cin>>c;
+    return leave("nornal leave");
 }
 
