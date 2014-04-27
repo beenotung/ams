ceb7fb4135a66120b996a7f32fb901ac0eecea8a
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-04-27 17:26:11 +0800

    testing ID

--- AMS.cpp
+++ AMS.cpp
@@ -577,10 +577,26 @@ void loadrecfile()
     string ID,Name,Pro;
     float GPA;
     int Num;
-    while (sturecfile>>ID>>Name>>GPA>>Num>>Pro)
-    {
-        ///add
+    string oldline="";
+    string newline="1";
+    do {
+        oldline=newline;
+        getline(sturecfile,newline);
+        int i;
+        int i1=0;
+        int i2=0;
+        while (newline[i1]!='\t') {i1++;}
+        ID="";
+        char tmp[9];
+        for (int j=0;j<i;j++){
+            tmp[j]=newline[j];
+        }
+        tmp[i]='\0';
+        ID=tmp;
+        while (newline[i]!='\t') {i++;}
+        cout<<endl<<'*'<<ID<<'*';
     }
+    while (newline!=oldline);
 }
 
 
